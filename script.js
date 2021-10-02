b47213ae2105022d7a3691c8796b10204cbc0c83
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-10-02 16:36:12 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-var cartaPaulo = {
-  nome: "Shiryu de dragão",
-  imagem:
-    "http://pm1.narvii.com/6399/96fdb9d4fe6a9e72b9bc60ad418e3c43795e53b4_00.jpg",
-  atributos: {
+let cartaPaulo = {
+  name: "Shiryu de dragão",
+  image: "http://pm1.narvii.com/6399/96fdb9d4fe6a9e72b9bc60ad418e3c43795e53b4_00.jpg",
+  atributes: {
     ataque: 5,
     defesa: 9,
     magia: 10
   }
 };
 
-var cartaRafa = {
-  nome: "Bulbasauro",
-  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-  atributos: {
+let cartaRafa = {
+  name: "Bulbasauro",
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+  atributes: {
     ataque: 7,
     defesa: 8,
     magia: 6
   }
 };
 
-var cartaGui = {
-  nome: "Darth Vader",
-  imagem:
+let cartaGui = {
+  name: "Darth Vader",
+  image:
     "https://images-na.ssl-images-amazon.com/images/I/41i-0NH0q9L._SX328_BO1,204,203,200_.jpg",
-  atributos: {
+  atributes: {
     ataque: 9,
     defesa: 8,
     magia: 2
   }
 };
 
-var cartaMaquina;
-var cartaJogador;
-var cartas = [cartaPaulo, cartaRafa, cartaGui];
+let cartaMaquina;
+let cartaJogador;
+let cartas = [cartaPaulo, cartaRafa, cartaGui];
 // 0          1           2
 
 function sortearCarta() {
-  var numeroCartaMaquina = parseInt(Math.random() * 3);
+  let numeroCartaMaquina = parseInt(Math.random() * 3);
   cartaMaquina = cartas[numeroCartaMaquina];
 
-  var numeroCartaJogador = parseInt(Math.random() * 3);
+  let numeroCartaJogador = parseInt(Math.random() * 3);
   while (numeroCartaJogador == numeroCartaMaquina) {
     numeroCartaJogador = parseInt(Math.random() * 3);
   }
@@ -51,9 +50,9 @@ function sortearCarta() {
   exibirCartaJogador();
 }
 
-function obtemAtributoSelecionado() {
-  var radioAtributo = document.getElementsByName("atributo");
-  for (var i = 0; i < radioAtributo.length; i++) {
+function obtematributeselecionado() {
+  let radioAtributo = document.getElementsByName("atributo");
+  for (let i = 0; i < radioAtributo.length; i++) {
     if (radioAtributo[i].checked) {
       return radioAtributo[i].value;
     }
@@ -62,17 +61,17 @@ function obtemAtributoSelecionado() {
 
 function jogar() {
   console.log("chamou");
-  var atributoSelecionado = obtemAtributoSelecionado();
-  var divResultado = document.getElementById("resultado");
+  let atributeselecionado = obtematributeselecionado();
+  let divResultado = document.getElementById("resultado");
 
   if (
-    cartaJogador.atributos[atributoSelecionado] >
-    cartaMaquina.atributos[atributoSelecionado]
+    cartaJogador.atributes[atributeselecionado] >
+    cartaMaquina.atributes[atributeselecionado]
   ) {
     htmlResultado = "<p class='resultado-final'>Venceu</p>";
   } else if (
-    cartaJogador.atributos[atributoSelecionado] <
-    cartaMaquina.atributos[atributoSelecionado]
+    cartaJogador.atributes[atributeselecionado] <
+    cartaMaquina.atributes[atributeselecionado]
   ) {
     htmlResultado = "<p class='resultado-final'>Perdeu</p>";
   } else {
@@ -85,58 +84,56 @@ function jogar() {
 }
 
 function exibirCartaJogador() {
-  var divCartaJogador = document.getElementById("carta-jogador");
-  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
-  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
-  var moldura =
+  let divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.image})`;
+  let moldura =
     '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
-  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  let tagHTML = "<div id='opcoes' class='carta-status'>";
 
-  var opcoesTexto = "";
-  for (var atributo in cartaJogador.atributos) {
+  let opcoesTexto = "";
+  for (let atributo in cartaJogador.atributes) {
     opcoesTexto +=
       "<input type='radio' name='atributo' value='" +
       atributo +
       "'>" +
       atributo +
       " " +
-      cartaJogador.atributos[atributo] +
+      cartaJogador.atributes[atributo] +
       "<br>";
   }
-  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  let name = `<p class="carta-subtitle">${cartaJogador.name}</p>`;
 
-  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+  divCartaJogador.innerHTML = moldura + name + tagHTML + opcoesTexto + "</div>";
 }
 
 function exibirCartaMaquina() {
-  var divCartaMaquina = document.getElementById("carta-maquina");
-  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
-  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
-  var moldura =
+  let divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.image})`;
+  let moldura =
     '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
-  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  let tagHTML = "<div id='opcoes' class='carta-status'>";
 
-  var opcoesTexto = "";
-  for (var atributo in cartaMaquina.atributos) {
+  let opcoesTexto = "";
+  for (let atributo in cartaMaquina.atributes) {
     opcoesTexto +=
       "<p type='text' name='atributo' value='" +
       atributo +
       "'>" +
       atributo +
       " " +
-      cartaMaquina.atributos[atributo] +
+      cartaMaquina.atributes[atributo] +
       "</p>";
   }
-  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  let name = `<p class="carta-subtitle">${cartaMaquina.name}</p>`;
 
-  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+  divCartaMaquina.innerHTML = moldura + name + tagHTML + opcoesTexto + "</div>";
 }
 
 
 /*   Verificar o que acontece caso você não selecione nenhum dos 
-atributos e como solucionar
+atributes e como solucionar
 
-Adicionar a imagem do personagem assim que você selecionar a carta 
+Adicionar a image do personagem assim que você selecionar a carta 
 dele 
 
 Criar de fato um baralho, com várias outras cartas
